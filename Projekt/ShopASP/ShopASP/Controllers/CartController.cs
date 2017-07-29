6afdbc054f2e74c1217520212ffeb92fbca06f63
Rafał Nowak <rafalnowak1994@gmail.com>
2017-07-29 19:52:34 +0200

    Add some

---
+++ Projekt/ShopASP/ShopASP/Controllers/CartController.cs
@@ -0,0 +1,40 @@
+﻿using ShopASP.DAL;
+using ShopASP.Infastructure;
+using ShopASP.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopASP.Controllers
+{
+    public class CartController : Controller
+    {
+        private CartManager cartManager;
+        private ISessionFuncs sessionManager { get; set; }
+        private ShopContext db = new ShopContext();
+        public CartController()
+        {
+            this.sessionManager = new defaultSession();
+            this.cartManager = new CartManager(this.sessionManager, this.db);
+        }
+        // GET: Cart
+
+        public ActionResult AddToCart(int id)
+        {
+            //cartManager.AddToCart(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public ActionResult Index()
+        {
+            // cartItems = cartManager.GetCart();
+            // var cartTotalPrice = cartManager.GetCartTotalPrice();
+
+            // CartViewModel cartVM = new CartViewModel() { CartItems = cartItems, TotalPrice = cartTotalPrice };
+
+            return View();
+        }
+    }
+}
---
+++ Projekt/ShopASP/ShopASP/Models/Size.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopASP.Models
+{
+    public class Size
+    {
+        public int SizeId { get; set; }
+        public string ItemSize { get; set; }
+    }
+}
